a51a3743dbe03fb7c0643241f5e43ff60201b8fc
Yolanda Feng <yuf019@ucsd.edu>
2025-01-23 23:56:25 -0800

    3.1

--- global.js
+++ global.js
@@ -4,11 +4,23 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a")
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-currentLink.classList.add('current');
-if (currentLink) {
-  currentLink.classList.add('current');
+let pages = [
+  { url: 'index.html', title: 'Home' },
+  { url: 'Projects/index.html', title: 'Projects' },
+  { url: 'Resume/index.html', title: 'Resume' },
+  { url: 'Contact/index.html', title: 'Contact' },
+  { url: 'https://github.com/YolandaFYL', title: 'Github Profile' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
