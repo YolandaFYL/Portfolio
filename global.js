718c1232ea760e1e7efc814fc333e70c62a4f313
Yolanda Feng <yuf019@ucsd.edu>
2025-01-30 23:35:46 -0800

    4

--- global.js
+++ global.js
@@ -70,3 +70,16 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = selectedScheme;
   console.log('Color scheme changed to', selectedScheme);
 });
+
+export async function fetchJSON(url) {
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data; 
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
